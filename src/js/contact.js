5aede8b7659a1bc61634d77a15d287cb5b2b3a54
Aleksandr <montoevaleks@mail.ru>
2024-09-10 09:11:20 +0300

    Created contact:hero and added contact:partners

---
+++ src/js/contact.js
@@ -0,0 +1,11 @@
+import '../assets/scss/contact.scss';
+
+// components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+
+
+useTheme();
+useBurger();
+
+
