ad4081789db77d1ca06fd96efad7f9e75a6b1c3d
Aleksandr <montoevaleks@mail.ru>
2024-10-24 13:34:06 +0300

    Created Detail-blog

---
+++ src/js/detail-blog.js
@@ -0,0 +1,11 @@
+import '/scss/detail-blog.scss';
+
+// components
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+
+useTheme();
+useBurger();
+
+
+
